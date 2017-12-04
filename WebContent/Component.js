736feec58bc08de52efd54fdb354bc7a8336d851
thilo.jahnke <thilo.jahnke@bechtle.com>
2017-12-04 14:07:04 +0100

    with new default model

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -1,30 +1,27 @@
 /**
  * 
  */
-sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/model/resource/ResourceModel",
-	"sap/ui/model/json/JSONModel"
-],function(UIComponent,ResourceModel,JSONModel){
-	"use strict";
-	return UIComponent.extend("WT4.Component",{
-		metadata : {
-			manifest : "json"
-		},
-		init: function(){
-			UIComponent.prototype.init.apply(this,arguments);
-			var i18nModel = new ResourceModel({
-				bundleName:"WT4.i18n.i18n"
-			}); 
-			this.setModel(i18nModel,"i18n");
-			var oJModelData = { Adress : { name1 : "John"  ,
-					            name2 : "Doom" 
-					            	}
-								};
-			var oJModel = new JSONModel( oJModelData );
-			this.setModel(oJModel);
-					 
-					
-		}
-	});
-}); 
+sap.ui.define([ "sap/ui/core/UIComponent",
+		"sap/ui/model/resource/ResourceModel", "sap/ui/model/json/JSONModel" ],
+		function(UIComponent, ResourceModel, JSONModel) {
+			"use strict";
+			return UIComponent.extend("WT4.Component", {
+				metadata : {
+					manifest : "json"
+				},
+				init : function() {
+					UIComponent.prototype.init.apply(this, arguments);
+					var i18nModel = new ResourceModel({
+						bundleName : "WT4.i18n.i18n"
+					});
+					this.setModel(i18nModel, "i18n");
+					var oJModelData = {
+						Adress : {
+							name1 : "John",
+							name2 : "Doom"
+						}
+					}
+                   this.setModel(new JSONModel(oJModelData));
+				}
+			});
+		});
